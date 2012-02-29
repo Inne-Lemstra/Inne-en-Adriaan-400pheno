8d35fd4075566b46e81dde5c72a7d1653dd35ff1
Adriaan van der Graaf <adriaan.vd.graaf@gmail.com>
2012-02-29 12:01:58 +0100

    initial import of z score function

---
+++ R/Z-score.R
@@ -0,0 +1,14 @@
+## copyright Adriaan van der Graaf 2012
+# Z-score function
+
+
+# 
+#input een vector 
+z.vector     <- function(a){
+  #dit gedeelte is voor de vectorfunctie
+  vecmean <- mean(a,na.rm=T)
+  vecz <- NULL
+  for(i in 1:length(a)){vecz <- c(vecz, ((a[i]-vecmean)/sd(a,na.rm=T)))} #hier wordt de z score uitgerekend voor elke waarde in een vector. en in de vector vecz gestopt
+  vecz
+}
+# 
