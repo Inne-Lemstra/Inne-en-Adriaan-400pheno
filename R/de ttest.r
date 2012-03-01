0bd4c60fa9919d8a1a72fbad7438da10354e75ff
DannyArends <Danny.Arends@gmail.com>
2012-03-01 16:43:38 +0100

    Variabele genotype coding

--- R/de ttest.r
+++ R/de ttest.r
@@ -2,13 +2,13 @@
 # created: 01-03-12
 #(c) created by Inne Lemstra
 
-Peas<- function(MatrixTrait, MatrixGeno, conf.level = 0.95){
+Peas<- function(MatrixTrait, MatrixGeno, genocode = c("AA","BB"), conf.level = 0.95){
   matru<-NULL
   for(rmark in 1:ncol(MatrixGeno)){
     vectu<-NULL
     for(b in 1:ncol(MatrixTrait)){
-	    genoA <- which(MatrixGeno[,rmark]=="AA")		#AA genotype selecteren uit col marker
-	    genoB <- which(MatrixGeno[,rmark]=="BB")		#BB genotype selecteren uit col marker
+	    genoA <- which(MatrixGeno[,rmark]==genocode[1])		#AA genotype selecteren uit col marker
+	    genoB <- which(MatrixGeno[,rmark]==genocode[2])		#BB genotype selecteren uit col marker
       coru <- t.test(MatrixTrait[genoA,b],MatrixTrait[genoB,b], conf.level = conf.level, use="pair")$p.value
       vectu<-c(vectu,coru)
     }
@@ -21,3 +21,4 @@ Peas<- function(MatrixTrait, MatrixGeno, conf.level = 0.95){
 
 #om te gebruiken store Peas in een variabele
 #vb.	T1 <- Peas(phenotypes, genotypes, 0.95)
+#vb.	T1 <- Peas(phenotypes, genotypes, c("CC","DD"), 0.95)
