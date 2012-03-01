619164747c37ee8cc98fd76f647363cfd76a4244
Inne-Lemstra <innelemstra@msn.com>
2012-03-01 12:36:46 +0100

    P.value calculator is now a function

--- R/de ttest.r
+++ R/de ttest.r
@@ -1,6 +1,11 @@
-#t.test doen
+#t.test doen en P-values uitzetten
 # created: 01-03-12
 #(c) created by Inne Lemstra
+
+Peas<- function(MatrixTrait,MatrixGeno,Clevel){
+phenotypes <- MatrixTrait 
+genotypes <- MatrixGeno
+C <- Clevel											#confidence level
 
 matru<-NULL
 for(rmark in 1:ncol(genotypes)){
@@ -8,12 +13,16 @@ for(rmark in 1:ncol(genotypes)){
 	for(b in 1:ncol(phenotypes)){
 	    genoA <- which(genotypes[,rmark]=="AA")		#AA genotype selecteren uit col marker
 	    genoB <- which(genotypes[,rmark]=="BB")		#BB genotype selecteren uit col marker
-		coru <- t.test(phenotypes[genoA,b],phenotypes[genoB,b], conf.level = 0.95, use="pair")$p.value
+		coru <- t.test(phenotypes[genoA,b],phenotypes[genoB,b], conf.level = C, use="pair")$p.value
 		vectu<-c(vectu,coru)
 	}
 	
 	matru<- cbind(matru,vectu)
 }
 
-colnames(matru)<- colnames(genotypes)
-plot(-log10(matru)[1,],t='l')
+	colnames(matru)<- colnames(genotypes)
+	Result <- -log10(matru)
+}
+
+#om te gebruiken store Peas in een variabele
+#vb.	T1 <- Peas(phenotypes, genotypes, 0.95)
