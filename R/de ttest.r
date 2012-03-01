3706478bd636fe7eaecb21ce59bbb77647f4ac20
DannyArends <Danny.Arends@gmail.com>
2012-03-01 16:36:10 +0100

    Some more fixing

--- R/de ttest.r
+++ R/de ttest.r
@@ -2,24 +2,21 @@
 # created: 01-03-12
 #(c) created by Inne Lemstra
 
-Peas<- function(MatrixTrait, MatrixGeno, Clevel = 0.95){
-C <- Clevel											#confidence level
-
-matru<-NULL
-for(rmark in 1:ncol(MatrixGeno)){
-	vectu<-NULL
-	for(b in 1:ncol(MatrixTrait)){
+Peas<- function(MatrixTrait, MatrixGeno, conf.level = 0.95){
+  matru<-NULL
+  for(rmark in 1:ncol(MatrixGeno)){
+    vectu<-NULL
+    for(b in 1:ncol(MatrixTrait)){
 	    genoA <- which(MatrixGeno[,rmark]=="AA")		#AA genotype selecteren uit col marker
 	    genoB <- which(MatrixGeno[,rmark]=="BB")		#BB genotype selecteren uit col marker
-		coru <- t.test(MatrixTrait[genoA,b],MatrixTrait[genoB,b], conf.level = C, use="pair")$p.value
-		vectu<-c(vectu,coru)
-	}
-	
-	matru<- cbind(matru,vectu)
-}
+      coru <- t.test(MatrixTrait[genoA,b],MatrixTrait[genoB,b], conf.level = conf.level, use="pair")$p.value
+      vectu<-c(vectu,coru)
+    }
+    matru<- cbind(matru,vectu)
+  }
 
 	colnames(matru)<- colnames(MatrixGeno)
-	Result <- -log10(matru)
+	return(-log10(matru))
 }
 
 #om te gebruiken store Peas in een variabele
