3abf84cee3bcec9ed2636541d30c4f7aac54c8fd
Inne-Lemstra <innelemstra@msn.com>
2012-03-01 14:20:20 +0100

    standaart deviation werkt nu

--- R/grebbing groups.r
+++ R/grebbing groups.r
@@ -4,29 +4,37 @@
 #(c) created by Inne Lemstra
 
 #eigenschappen
-Gmax <-grep("Gmax", colnames(phenotypes), ignore.case = TRUE)
-U8<-grep("U8416", colnames(phenotypes), ignore.case = TRUE)
-T10<-grep("T10", colnames(phenotypes), ignore.case = TRUE)
-T50<-grep("T50", colnames(phenotypes), ignore.case = TRUE)
-AUC<-grep("AUC", colnames(phenotypes), ignore.case = TRUE)
+Gmax 	<-	grep("Gmax", colnames(phenotypes), ignore.case = TRUE)
+U8		<-	grep("U8416", colnames(phenotypes), ignore.case = TRUE)
+T10		<-	grep("T10", colnames(phenotypes), ignore.case = TRUE)
+T50		<-	grep("T50", colnames(phenotypes), ignore.case = TRUE)
+AUC		<-	grep("AUC", colnames(phenotypes), ignore.case = TRUE)
 
 #eigenschappen zonder stratification
-Gmaxc <-Gmax[-which(grep("Gmax", colnames(phenotypes), ignore.case = TRUE) %in% grep("Stratification", colnames(phenotypes), ignore.case=TRUE))]
-U8c<-U8[-which(grep("U8416", colnames(phenotypes), ignore.case = TRUE) %in% grep("Stratification", colnames(phenotypes), ignore.case=TRUE))]
-T10c<-T10[-which(grep("T10", colnames(phenotypes), ignore.case = TRUE) %in% grep("Stratification", colnames(phenotypes), ignore.case=TRUE))]
-T50c<-T50[-which(grep("T50", colnames(phenotypes), ignore.case = TRUE) %in% grep("Stratification", colnames(phenotypes), ignore.case=TRUE))]
-AUCc<-AUC[-which(grep("AUC", colnames(phenotypes), ignore.case = TRUE) %in% grep("Stratification", colnames(phenotypes), ignore.case=TRUE))]
+Gmaxc 	<-	Gmax[-which(grep("Gmax", colnames(phenotypes), ignore.case = TRUE) %in% grep("Stratification", colnames(phenotypes), ignore.case=TRUE))]
+U8c		<-	U8[-which(grep("U8416", colnames(phenotypes), ignore.case = TRUE) %in% grep("Stratification", colnames(phenotypes), ignore.case=TRUE))]
+T10c	<-	T10[-which(grep("T10", colnames(phenotypes), ignore.case = TRUE) %in% grep("Stratification", colnames(phenotypes), ignore.case=TRUE))]
+T50c	<-	T50[-which(grep("T50", colnames(phenotypes), ignore.case = TRUE) %in% grep("Stratification", colnames(phenotypes), ignore.case=TRUE))]
+AUCc	<-	AUC[-which(grep("AUC", colnames(phenotypes), ignore.case = TRUE) %in% grep("Stratification", colnames(phenotypes), ignore.case=TRUE))]
 
 #boxplot
-mGmax<- boxplot(as.matrix(phenotypes[,Gmax]),na.rm=TRUE)
-mU8<- boxplot(as.matrix(phenotypes[,U8]),na.rm=TRUE)
-mT10<- boxplot(as.matrix(phenotypes[,T10]),na.rm=TRUE)
-mT50<- boxplot(as.matrix(phenotypes[,T50]),na.rm=TRUE)
-mAUC<- boxplot(as.matrix(phenotypes[,AUC]),na.rm=TRUE)
+bGmax	<- 	boxplot(as.matrix(phenotypes[,Gmax]),na.rm=TRUE)
+bU8		<- 	boxplot(as.matrix(phenotypes[,U8]),na.rm=TRUE)
+bT10	<- 	boxplot(as.matrix(phenotypes[,T10]),na.rm=TRUE)
+bT50	<- 	boxplot(as.matrix(phenotypes[,T50]),na.rm=TRUE)
+bAUC	<-	boxplot(as.matrix(phenotypes[,AUC]),na.rm=TRUE)
 
-#standart deviation (werkt niet)
-sdGmax<-sd(as.matrix(phenotypes[,Gmaxc]),na.rm=TRUE)
-sdU8<- mean(apply(phenotypes[,U8c], 2, na.rm=TRUE, mean))
-sdT10<- mean(apply(phenotypes[,T10c], 2,na.rm=TRUE, mean))
-sdT50<- mean(apply(phenotypes[,T50c], 2, na.rm=TRUE, mean))
-sdAUC<- mean(apply(phenotypes[,AUCc], 2, na.rm=TRUE, mean))
+#mean van means
+mGmax	<-  mean(apply(phenotypes[,Gmaxc], 2, na.rm=TRUE, mean))
+mdU8	<- 	mean(apply(phenotypes[,U8c], 2, na.rm=TRUE, mean))
+mdT10	<- 	mean(apply(phenotypes[,T10c], 2,na.rm=TRUE, mean))
+mdT50	<- 	mean(apply(phenotypes[,T50c], 2, na.rm=TRUE, mean))
+mdAUC	<- 	mean(apply(phenotypes[,AUCc], 2, na.rm=TRUE, mean))
+
+#standart deviation
+sdGmax 	<- 	sd(apply(as.matrix(phenotypes[,Gmaxc]),2, na.rm=TRUE, sd))
+sdU8	<- 	sd(apply(as.matrix(phenotypes[,U8c]),2, na.rm=TRUE, sd))
+sdT10	<- 	sd(apply(as.matrix(phenotypes[,T10c]),2, na.rm=TRUE, sd))
+sdT50	<- 	sd(apply(as.matrix(phenotypes[,T50c]),2, na.rm=TRUE, sd))
+sdAUC	<- 	sd(apply(as.matrix(phenotypes[,AUCc]),2, na.rm=TRUE, sd))
+
