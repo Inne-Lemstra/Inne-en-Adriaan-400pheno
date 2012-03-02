f36e2d7185b3e3393e9b24d1135376354c2a9c23
Adriaan van der Graaf <adriaan.vd.graaf@gmail.com>
2012-03-02 15:36:39 +0100

    naam aangepast van de.ttest.R

---
+++ R/de.ttest.r
@@ -0,0 +1,25 @@
+#t.test doen en P-values uitzetten
+# created: 01-03-12
+#(c) created by Inne Lemstra
+
+t.test.mat<- function(MatrixTrait, MatrixGeno, genocode = c("AA","BB"), conf.level = 0.95){
+  matru<-NULL
+  for(rmark in 1:ncol(MatrixGeno)){
+    vectu<-NULL
+    for(b in 1:ncol(MatrixTrait)){
+	    genoA <- which(MatrixGeno[,rmark]==genocode[1])		#AA genotype selecteren uit col marker
+	    genoB <- which(MatrixGeno[,rmark]==genocode[2])		#BB genotype selecteren uit col marker
+      coru <- t.test(MatrixTrait[genoA,b],MatrixTrait[genoB,b], conf.level = conf.level, use="pair")$p.value
+      vectu<-c(vectu,coru)
+    }
+    matru<- cbind(matru,vectu)
+  }
+
+	colnames(matru)<- colnames(MatrixGeno)
+	rownames(matru) <- colnames(MatrixTrait)
+	return(-log10(matru)) #levert de min 10 log van de p waarde.
+}
+
+#om te gebruiken store t.test.mat in een variabele
+#vb.	T1 <- t.test.mat(phenotypes, genotypes, 0.95)
+#vb.	T1 <- t.test.mat(phenotypes, genotypes, c("CC","DD"), 0.95)
