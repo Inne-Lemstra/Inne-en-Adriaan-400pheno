81e6e18f18ca6404d485ebb7fcdb3bc66c691388
Adriaan van der Graaf <adriaan.vd.graaf@gmail.com>
2012-03-02 11:54:31 +0100

    hier is de functie effect.matrix. Voer genotypen en phenotypen resp. in en krijg er de verhoudingen tussen AA en BB in

---
+++ R/effect.matrix.R
@@ -0,0 +1,27 @@
+##copyrigh Adriaan van der Graaf 2012
+#functie voor het maken van een effect matrix
+
+#nu de effect waarde uitrekenen. 
+#omdat de waarden niet in een object zitten, moet ik ze er zelf even uittrekken.
+
+createEffectMatrix <- function(genotypes,phenotypes){  #eerst genotypen en daarna phenotypen invullen. Dit geeft de effecten AA/BB op gemiddelden
+  meanAA <- matrix(0,ncol(phenotypes),ncol(genotypes)) 
+  meanBB <- matrix(0,ncol(phenotypes),ncol(genotypes))
+    for(i in 1:ncol(genotypes)){
+	  for(b in 1:ncol(phenotypes)){
+	    genoA <- which(genotypes[,i]=="AA")   #in deze loop wordt het gemiddelde van een phenotype met genotype AA en BB berekend
+	    genoB <- which(genotypes[,i]=="BB")   
+        meanAA[b,i] <- mean(phenotypes[genoA,b],na.rm=T) #hier wordt de gemiddelde in een matrix geplaatst met de hoeveelheid phenotypen als rij, en de hoeveelheid genotypen als kolom
+	    meanBB[b,i] <- mean(phenotypes[genoB,b],na.rm=T)
+	  }
+    }
+ effectAAdivBB <- matrix(0,ncol(phenotypes),ncol(genotypes))   #nu de verhouding berekenen.
+ colnames(effectAAdivBB) <- colnames(genotypes)					#voor betere herkenning de namen even gelijktrekken
+ rownames(effectAAdivBB) <- colnames(phenotypes)
+   for(i in 1:nrow(effectAAdivBB)){                     #in deze loop worden de gemiddelden 	door elkaar gedeeld
+	 for(n in 1:ncol(effectAAdivBB)){
+	   effectAAdivBB[i,n] <- ((meanAA[i,n])/(meanBB[i,n])) #hier vindt de deel-actie plaats voor alle waarden in de matrix
+     }
+   }
+   effectAAdivBB
+}
