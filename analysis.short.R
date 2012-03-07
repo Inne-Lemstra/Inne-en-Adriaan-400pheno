fbfa3af610f9516dd982172d6d1927e458c61590
Inne-Lemstra <innelemstra@msn.com>
2012-03-07 12:01:03 +0100

    De analysis is bijgewerkt tot aan het smaenvoegen van de T.test en Anova matrices

--- analysis.short.R
+++ analysis.short.R
@@ -7,6 +7,7 @@ source("400pheno/R/effect.matrix.R")
 source("400pheno/R/t.test.R")
 source("400pheno/R/trait.marker.list.R")
 source("400pheno/R/anova.mat.R")
+source("400pheno/R/T en A matrix.r")
 
 #data laden
 data <- read.csv("BayShatraitsAll.csv",sep=";")
@@ -17,7 +18,7 @@ genotypes <- genotypes[-c(1,2),]
 genotypes <- apply(genotypes,2,as.character)
 ##einde data laden
 
-
+MatrixT.test<-NULL
 tmat <- t.test.mat(phenotypes,genotypes)  #tmat is een matrix van alle -log10 p-waarden per genotype tov phenotype
 MatrixT.test <- trait.marker.list(peak.mat.row(tmat,3,phenotypes)) #MatrixT.test is een matrix van alle traits en markers waar de piek groter is dan 3. in de eerste kolom trait, 2e kolom marker.
 
@@ -28,8 +29,8 @@ for (i in 1:nrow(MatrixT.test)){
 }
 MatrixT.test <- cbind(MatrixT.test,LODT.test) #hier wordt de LODT.test waarde aan de trait matrix gebonden.
 
-
-effect.mat<- effect.matrix(genotypes,phenotypes) #effect matrix is de matrix met alle AA/BB waarden.
+#AA/BB
+effect.mat<- effect.matrix(genotypes,phenotypes,"divide") #effect matrix is de matrix met alle AA/BB waarden.
 effect.vec <- NULL
 for (i in 1:nrow(MatrixT.test)){
    effect.vec <- c(effect.vec,effect.mat[MatrixT.test[i,1],MatrixT.test[i,2]]) #wordt automatisch op volgorde gezet door de MatrixT.testrix. Deze vraagt de trait en marker namen op en de waarde is de AAdivBB.
@@ -37,3 +38,40 @@ for (i in 1:nrow(MatrixT.test)){
 MatrixT.test <- cbind(MatrixT.test,effect.vec) #hier de effect.vec vector aan de MatrixT.test gebonden.
 
 colnames(MatrixT.test) <- c("Trait", "Marker", "LODT.test", "AA/BB") #colnames nog even gelijktrekken.
+
+#AA-BB
+effect.mat<-NULL
+effect.mat<- effect.matrix(genotypes,phenotypes,"substract") #effect matrix is de matrix met alle AA/BB waarden.
+effect.vec <- NULL
+for (i in 1:nrow(MatrixT.test)){
+   effect.vec <- c(effect.vec,effect.mat[MatrixT.test[i,1],MatrixT.test[i,2]]) #wordt automatisch op volgorde gezet door de MatrixT.testrix. Deze vraagt de trait en marker namen op en de waarde is de AAdivBB.
+}
+MatrixT.test <- cbind(MatrixT.test,effect.vec) #hier de effect.vec vector aan de MatrixT.test gebonden.
+
+colnames(MatrixT.test) <- c("Trait", "Marker", "LODT.test", "AA/BB","AA-BB") #colnames nog even gelijktrekken.
+
+#Matrix LOD Anavo
+
+anovamat <- anova.mat(as.matrix(phenotypes),as.matrix(genotypes))
+MatrixAnova <- trait.marker.list(peak.mat.row(anovamat,3,phenotypes))
+
+
+#LODAnova tmat waarden die groter zijn dan de cutoff.
+LODAnova <- NULL
+for (i in 1:nrow(MatrixAnova)){
+  LODAnova <- c(LODAnova, anovamat[MatrixAnova[i,1],MatrixAnova[i,2]])  #wordt automatisch op volgorde gezet door de MatrixAnovarix. deze vraagt de trait en marker namen op en de waarde in de tmat
+}
+MatrixAnova <- cbind(MatrixAnova,LODAnova) #hier wordt de LODAnova waarde aan de trait matrix gebonden.
+colnames(MatrixAnova) <- c("Trait", "Marker", "LODAnova") #colnames nog even gelijktrekken.
+
+#Complte matrix met t.test en anova
+Temp<-voegsamen(MatrixT.test,MatrixAnova)
+Order<-sort(colnames(Temp),decreasing=TRUE)
+CombinedLod<-Temp[Order]
+
+
+
+
+
+
+
