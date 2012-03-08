d7e747bb09cea31996debffd04e0f616ea9d859f
Inne-Lemstra <innelemstra@msn.com>
2012-03-08 13:31:26 +0100

    vervangt nu alle andere waarden door NA

--- R/M.matcher.r
+++ R/M.matcher.r
@@ -3,10 +3,15 @@
 
 #een functie die een collum in een matrix met een andere kan vergelijke en er een nieuwe matrix van maakt
 
-matrixA<-grep.term.col(phenotypes,"ABA")
-Refmatrix<-phenotypes
+#Refmatrix is een grote matrix waarin de colnames gevonden moeten worden
+#zoekmatrix is een kleinere matrix met colnames die in Refmatrix gevonden moeten worden
+#Term is een woord waarmee je de waarden van alle gevonden kollums vervangt
 
 M.matcher<-function(Refmatrix, zoekmatrix, Term){
+if(missing(Refmatrix)) stop("ReferentieMatrix is not found")
+if(missing(zoekmatrix)) stop("Vul een matrix in met zoektermen")
+if(ncol(zoekmatrix)>ncol(Refmatrix)) stop("Refmatrix moet groter zijn de zoekmatrix")
+if(missing(Term)) stop("vul een Term in die de waarden vervangt")
 Names<-NULL
 matcol<-NULL
 for(x in 1:ncol(zoekmatrix)){
@@ -21,16 +26,25 @@ matcol<-c(matcol,matchcol)
 vec<- 1:nrow(Refmatrix)
 vec[1:nrow(Refmatrix)]<-Term
 Refmatrix[,matcol] <- vec
+Invmatcol<-(which((1:ncol(Refmatrix)%in%matcol)==FALSE))
+vec[1:nrow(Refmatrix)]<-"NA"
+Refmatrix[,Invmatcol]=vec
 Refmatrix
 }
 
+#Test
+#matrixA<-grep.term.col(phenotypes,"ABA")
+#data <- read.csv("BayShatraitsAll.csv",sep=";")
+#phenotypes <- data[,1:404]
+#T1<-M.matcher(phenotypes,matrixA, "ABA")
 
-T1<-matrix(matcol,1,length(matcol))
-colnames(T1)<-Names
+#extra
+#T1<-matrix(matcol,1,length(matcol))
+#colnames(T1)<-Names
 
-T2<- as.matrix(matcol)
-colnames(T2)<-Names
+#T2<- as.matrix(matcol)
+#colnames(T2)<-Names
 
 
-batchmat<- replace(matrixB[,matcol]
+
 
