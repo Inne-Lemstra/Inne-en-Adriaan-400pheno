136b1bcd67f4c4ecc771946d232a6525b886a902
Adriaan van der Graaf <adriaan.vd.graaf@gmail.com>
2012-03-08 11:42:45 +0100

    hier is de functie gemaakt.

--- R/Grep.term.col.R
+++ R/Grep.term.col.R
@@ -5,21 +5,30 @@ grep.term.col <- function(traits, term, cutoff=0){
   if(missing(traits)) stop("trait matrix needs to be defined")
   if(missing(term)) stop("term needs to be defined")
   if(!is.character(term)) stop("term needs to be a character")
+  for (i in 1:length(cutoff)) {if(cutoff[i] < 0) stop("Cutoff needs to be a whole number above zero.")}
   
   
   #functie begint werkelijk HIER
-  colnamesorigin <-colnames(traits)
-  if(cutoff == 0) {} else{ #voor het afkappen van de colnames. Alleen de laatste characters. voor de batches kan nog uitgebreid worden met tussen welke characters ik wil. Maar ik ga het niet doen.
+  colnamesorigin <-colnames(traits) #hier de colnames saven om later in de eventueel afgekapte colnames te stoppen
+  
+  if(cutoff[i] == 0) {} else{ #voor het afkappen van de colnames. Alleen de laatste characters. voor de batches kan nog uitgebreid worden met tussen welke characters ik wil. Maar ik ga het niet doen.
     for(i in 1:ncol(traits)){
       colnames(traits)[i] <- substr(colnames(traits)[i],(nchar(colnames(traits)[i])-(cutoff-1)),nchar(colnames(traits)[i])) #de laatste cutoff caracters geef ik.
     }
   }
-  colnamesorigin <-colnames(traits)
-  cols <- grep(term, colnames (traits))			#zoek naar de term in de colnames
-  searchmat <- NULL
-    for (i in 1:length(cols)){
-      searchmat <- cbind(searchmat, traits[,cols[i]])  # maak een matrix met de kolommen die de term bevatten in de colnaam
-    }
-  colnames(searchmat)<- colnamesorigin[cols]
-  return(searchmat) #geef de matrix
+  
+  searchmat <- vector("list",length(term)) #hier de lijst definieren.
+  for (n in 1:length(term)){ #als term langer is dan 1, dan krijgt de lijst meer opties.
+    
+	cols <- grep(term[n], colnames (traits))			#zoek naar de term in de colnames
+    if(length(cols) == 0) {next}
+      for (i in 1:length(cols)){
+        searchmat[[n]] <- cbind(searchmat[[n]], traits[,cols[i]])  # maak een matrix met de kolommen die de term bevatten in de colnaam
+      }
+    colnames(searchmat[[n]])<- colnamesorigin[cols] #colnames terugzetten voor het geval ze afgekapt waren.
+  }	
+  return(searchmat) #geef de lijst met matrices
 }
+
+
+batchlist <- grep.term.col(phenotypes,c(".A.",".B.",".C.",".ABC",".D."),5)
