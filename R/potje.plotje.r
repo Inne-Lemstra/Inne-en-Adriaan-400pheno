89eaafc360f0bf877082d85ac1d055ba6875c98e
Inne-Lemstra <innelemstra@msn.com>
2012-03-09 12:01:24 +0100

    Nu met een hele hoop nieuwe Features

--- R/potje.plotje.r
+++ R/potje.plotje.r
@@ -5,12 +5,48 @@
 
 #plot grote
 plot(c(0,10),c(0,10),type="n")
-plot(c(0,69),c(0,10),type="n", xlab="Markers",ylab="Number of LOD above cuttof",axes=FALSE)
+plot(c(0,length(xass)),c(1,107),type="n", xlab="Markers",ylab="Number of LOD above cuttof",axes=FALSE)
 
 #bekijk ?axis
 op <- par(las = 2)
 op<- par(cex = 0.6)
-op <- par(mai=c(2,0.4,0,0))
+op <- par(mai=c(1,0.8,1,0.5))
 
-axis(1,1:ncol(chromos), colnames(chromos))
+axis(2,0:107)
+X.maker(chromos,5,2)  
+points(yass)
+
+X.maker<- function(NameFile,aantalchr,gap){
+xass<-NULL
+for(x in 1:aantalchr){
+  if(x!=aantalchr){
+    vec<-NULL
+    vec<-colnames(NameFile)[which(NameFile==x)]
+    vec<-c(vec,rep(" ",gap))
+  }
+    else{
+    vec<-NULL
+    vec<-colnames(NameFile)[which(NameFile==x)]
+    }
+  xass<- c(xass,vec)
+  }
+  axis(1,1:length(xass), labels=xass)
+  }
+
+X.maker(chromos,5,2)  
+#nu de points invullen
+table(CombiMatrix[,2])
+
+yass<-NULL
+for(y in 1:length(xass)){
+  positie<-NULL
+  positie<-grep(xass[y],names(table(CombiMatrix[,2])))
+    if(!is.na(positie&&1)){
+    yass<-c(yass, table(CombiMatrix[,2])[positie])
+  }
+  else{
+    yass<-c(yass,0)
+  }
+  }
+
 
