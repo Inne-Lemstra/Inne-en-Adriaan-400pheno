5938b93cfbd0eef61f0b7ac979a11652794d332b
Inne-Lemstra <innelemstra@msn.com>
2012-03-15 16:40:21 +0100

    meer regels

--- R/sequencer.r
+++ R/sequencer.r
@@ -7,7 +7,11 @@ sequencer<- function(Matrix, lijst_voorwaarden,cuttoff)
 
 for(traits in 1:length(lijst_voorwaarden))
   the.rows<-lijst_voorwaarden[trait]==Matrix[,1]
-  
+  for(per.row in the.rows)
+    if(Matrix[per.row,6]>0 & Matrix[per.row,4]>cuttoff)AABB<-"A"
+    if(Matrix[per.row,6]<0 & Matrix[per.row,4]>cuttoff)AABB<-"B"
+    if(Matrix[per.row,6]==0 & Matrix[per.row,4]>cuttoff)AABB<-"A/B"
+    markAABB<-c(Matrix[per.row,2],AABB)
   if(Matrix[the.row,3]
   
   
