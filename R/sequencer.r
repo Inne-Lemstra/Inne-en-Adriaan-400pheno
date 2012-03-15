4df852cc78244a3457232b7f6928039f634ab2a7
Inne-Lemstra <innelemstra@msn.com>
2012-03-15 15:59:17 +0100

    eerste opzetje ABBAB  sequencer

---
+++ R/sequencer.r
@@ -0,0 +1,14 @@
+#(c)Inne Lemstra
+#date created: 15 maart
+
+#sequence maker
+
+sequencer<- function(Matrix, lijst_voorwaarden,cuttoff)
+
+for(traits in 1:length(lijst_voorwaarden))
+  the.rows<-lijst_voorwaarden[trait]==Matrix[,1]
+  
+  if(Matrix[the.row,3]
+  
+  
+  
