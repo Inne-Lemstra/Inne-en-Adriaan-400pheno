6e743acbe2f3455d8c9472ba0bddad82bddf51a4
Inne-Lemstra <innelemstra@msn.com>
2012-03-16 15:36:12 +0100

    functie om tweede as uit te zetten

--- R/potje.plotje.r
+++ R/potje.plotje.r
@@ -125,22 +125,12 @@ points(1:10, T1)
 
 axis(1,pretty(range(x),10))
 
-plotDanny <- function(Morgan, chromos, yass1, yass2, gapsize=25,type='l'){
+plotDanny <- function(Morgan, chromos, yass1, yass2=NULL, gapsize=25,type='l'){
   op <- par(las = 2)
   op <- par(cex.axis = 0.6)
   distances <- as.numeric(t(Morgan[1,]))
   chr <- as.numeric(unlist(t(chromos)))
   nchr <-length(unique(chr))
-    plot(c(0, getCD(nchr, gapsize=gapsize,distances=distances,chr=chr)),c((min(yass2)*1.25),max(yass2)*1.25),type="n", axes=F,,xlab="Markers", ylab="")
-  axis(4, c((min(yass2)*1.25),max(yass2)*1.25))
-  locs<-NULL
-  for(x in 1:nchr){
-    
-    locs <- c(locs,distances[which(chr==x)] + (getCD(x-1,gapsize,distances,chr)))
-      points(x=distances[which(chr==x)] + (getCD(x-1,gapsize,distances,chr)),y=yass2[which(chr==x)],t="p",pch=16,col="purple",lwd=3)
-   
-   }
-      par(new=T)
   plot(c(0, getCD(nchr, gapsize=gapsize,distances=distances,chr=chr)),c(0,max(yass1)*1.25),type="n",main="Summarized QTL plot", ylab="# of significant QTL", xlab="Markers",xaxt="n")
   axis(2, c(0,max(yass)*1.25))
   locs <- NULL
@@ -148,6 +138,20 @@ plotDanny <- function(Morgan, chromos, yass1, yass2, gapsize=25,type='l'){
     locs <- c(locs,distances[which(chr==x)] + (getCD(x-1,gapsize,distances,chr)))
     points(x=distances[which(chr==x)] + (getCD(x-1,gapsize,distances,chr)),y=yass1[which(chr==x)],type=type,col=x,lwd=3)
   }
+
+   if(!is.null(yass2)){
+      par(new=T)
+    plot(c(0, getCD(nchr, gapsize=gapsize,distances=distances,chr=chr)),c((min(yass2)*1.25),max(yass2)*1.25),type="n", axes=F,,xlab="Markers", ylab="")
+  axis(4, c((min(yass2)*1.25),max(yass2)*1.25),lwd=1)
+  locs<-NULL
+  for(x in 1:nchr){
+    
+    locs <- c(locs,distances[which(chr==x)] + (getCD(x-1,gapsize,distances,chr)))
+      points(x=distances[which(chr==x)] + (getCD(x-1,gapsize,distances,chr)),y=yass2[which(chr==x)],t="p",pch=16,col="purple",lwd=3)
+   
+   }
+  }
   axis(1,locs,labels=names(Morgan[1,]))
 }
 plotDanny(Morgan,chromos,yass1,sign(yass2))
+
